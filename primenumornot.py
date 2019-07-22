ae2ed19bccda9109aa8ec1d315d17cdde742ebc0
vikashmrv <53124092+vikashmrv@users.noreply.github.com>
2019-07-22 15:12:15 +0530

    Create primenumornot.py

---
+++ primenumornot.py
@@ -0,0 +1,11 @@
+    
+n=int(input())
+if (n>1): 
+   for i in range(2, n//2): 
+       if (n % i) == 0: 
+           print( "no") 
+           break
+   else: 
+       print("yes") 
+else: 
+   print("no")
